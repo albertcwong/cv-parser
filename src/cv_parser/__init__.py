57b38d772cb223c8381274d6f2b6ca95f282a85f
Albert Wong <albert.wong@salesforce.com>
2026-03-01 20:39:08 -0800

    remove cli code

--- src/cv_parser/__init__.py
+++ src/cv_parser/__init__.py
@@ -1,13 +1,1 @@
-"""Professor CV parser for business graduate schools."""
-
-from cv_parser.schemas import CVParseResult, ProfessorMetadata, Publication, Presentation, Recognition
-from cv_parser.parser import parse_cv
-
-__all__ = [
-    "CVParseResult",
-    "ProfessorMetadata",
-    "Publication",
-    "Presentation",
-    "Recognition",
-    "parse_cv",
-]
+"""CV Parser - batch notebook for extracting publications, presentations, recognitions from professor CVs."""
